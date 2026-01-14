911e773ae19ac9b3b55a9866fe01bd8d4e486333
Wren <155106259+wryneth@users.noreply.github.com>
2026-01-14 00:10:10 -0700

    Add landing page

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from "./components/Landing.vue";
 </script>
 
 <template>
-  <button class="btn">Button</button>
-  <h1 class="text-center">Diskette</h1>
+  <Landing />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+root {scroll-behavior: smooth;}
 </style>
