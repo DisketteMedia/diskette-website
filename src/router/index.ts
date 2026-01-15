63ae9d3c56f748059bbc8ac7d8e2f2c42a7abec5
Wren <155106259+wryneth@users.noreply.github.com>
2026-01-15 04:12:38 -0700

    Add titles to individual pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,41 @@ import Media from "../components/pages/Media.vue";
 import Glow from "../components/pages/Glow.vue";
 
 const routes = [
-    { path: '/kitchen', component: Kitchen, name: 'Kitchen'},
-    { path: '/media', component: Media, name: 'Media' },
-    { path: '/glow', component: Glow, name: 'Glow'},
+    {
+        path: '/',
+        name: 'Landing',
+        meta: {
+            title: 'Diskette.TV',
+            description: 'The home of the Diskette Kitchen, a collection of thoughtful, high quality videos about technology, from past to present.'
+        }
+    },
+    {
+        path: '/kitchen',
+        component: Kitchen,
+        name: 'Kitchen',
+        meta: {
+            title: 'Diskette Kitchen',
+            description: 'Thoughtful, high quality videos about technology, from past to present.'
+        }
+    },
+    {
+        path: '/media',
+        component: Media,
+        name: 'Media',
+        meta: {
+            title: 'Diskette Media',
+            description: 'Media production and products behind Diskette Kitchen'
+        }
+    },
+    {
+        path: '/glow',
+        component: Glow,
+        name: 'Glow',
+        meta: {
+            title: 'Diskette Glow',
+            description: 'The unique touch every room needs.'
+        }
+    },
 ];
 
 const router = createRouter({
@@ -17,6 +49,19 @@ const router = createRouter({
             el: '#content',
             top: 90,
         }
+    }
+});
+
+router.afterEach((to) => {
+    if (to.meta.title) {
+        let title;
+        if (typeof to.meta.title === 'function') {
+            title = to.meta.title(to);
+        }
+        else {
+            title = to.meta.title;
+        }
+        document.title = title;
     }
 });
 
