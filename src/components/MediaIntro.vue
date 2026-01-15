2b6ee0f406efd1397ce1ef73aa5dde6fe9db15d8
Wren <155106259+wryneth@users.noreply.github.com>
2026-01-14 21:04:05 -0700

    Add intro to DM section

---
+++ src/components/MediaIntro.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-full">
+    <div class="card bg-base-300 p-5 md:p-10 lg:p-5">
+      <div class="flex flex-col lg:flex-row gap-5">
+        <div class="grid grid-cols-2 place-items-center">
+          <img src="/images/portfolio/bike.webp" alt="bike equipment unloading" class="rounded-tl-md object-cover h-80">
+          <img src="/images/portfolio/tub.webp" alt="the bathroom with " class="rounded-tr-md object-cover h-80">
+          <img src="/images/portfolio/studio.webp" alt="diskette studio" class="rounded-bl-md object-cover h-80">
+          <img src="/images/portfolio/camSetup.webp" alt="Wren setting up a camera for shooting" class="rounded-br-md object-cover h-80">
+        </div>
+        <div class="pb-5 lg:p-10">
+          <img src="/mediaWide.svg" class="mb-5" alt="Diskette Kitchen logo">
+          <h2 class="text-4xl font-bold font-calistoga mb-5">Dedicated to the craft of video and </h2>
+          <p class="lg:w-200 md:w-full text-2xl sm:w-full flex flex-col gap-3 md:pe-20 pe-10 py-5 font-ubuntu">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem
+            placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
+            vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc
+            posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra
+            inceptos himenaeos.
+          </p>
+          <p class="text-xl font-ubuntu mt-5">Interested in a a partnership? Let's talk</p>
+          <a role="button" class="btn btn-neutral dark:btn-primary mt-5" href="#contact">Contact me</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/pages/Media.vue
+++ src/components/pages/Media.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import About from "../About.vue";
 import Contact from "../Contact.vue";
+import MediaIntro from "../MediaIntro.vue";
 </script>
 
 <template>
   <div class="mx-5 mb-5 flex flex-col gap-5">
+    <MediaIntro />
     <About />
-    <Contact />
+    <section id="contact">
+      <Contact />
+    </section>
   </div>
 </template>
 
