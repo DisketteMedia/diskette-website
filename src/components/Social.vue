b083aebe95f1c2c135b2c59e6722d90555b853c9
Wren <155106259+wryneth@users.noreply.github.com>
2026-01-14 21:42:50 -0700

    Fix props because I'm a fucking idiot

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
 
-const props = defineProps({
+defineProps({
   iconSize: {
     type: String,
-    default: '35px'
+    default: '25px'
   }
 })
 </script>
@@ -12,16 +12,16 @@ const props = defineProps({
 <template>
   <div class="flex join">
     <a role="button" class="btn btn-neutral dark:btn-primary join-item" href="https://youtube.com/@diskettekitchen">
-      <Icon :size="props.iconSize" icon="mdi:youtube"/>
+      <Icon :width="iconSize" :height="iconSize" icon="mdi:youtube"/>
     </a>
     <a role="button" class="btn btn-neutral dark:btn-primary join-item" href="https://bsky.app/profile/diskette.tv">
-      <Icon :size="props.iconSize" icon="ri:bluesky-fill"/>
+      <Icon :width="iconSize" :height="iconSize" icon="ri:bluesky-fill"/>
     </a>
     <a role="button" class="btn btn-neutral dark:btn-primary join-item" href="https://instagram.com/diskettekitchen">
-      <Icon :size="props.iconSize" icon="mdi:instagram"/>
+      <Icon :width="iconSize" :height="iconSize" icon="mdi:instagram"/>
     </a>
     <a role="button" class="btn btn-neutral dark:btn-primary join-item" href="https://printables.com/@diskette">
-      <Icon :size="props.iconSize" icon="simple-icons:printables"/>
+      <Icon :width="iconSize" :height="iconSize" icon="simple-icons:printables"/>
     </a>
   </div>
 </template>
