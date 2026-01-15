25497865af9eeaf5e4ff6056104e32e2df4311aa
Wren <155106259+wryneth@users.noreply.github.com>
2026-01-15 00:58:15 -0700

    Add email signup

--- src/components/pages/Glow.vue
+++ src/components/pages/Glow.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import GlowLanding from "../GlowLanding.vue";
 import CaseCompare from "../CaseCompare.vue";
+import MailSignup from "../MailSignup.vue";
 </script>
 
 <template>
-  <div class="mx-5 mb-5 flex flex-col gap-5">
-    <GlowLanding/>
-    <section id="glow">
+  <div class="mx-5 mb-5">
+    <GlowLanding class="mb-5"/>
+    <section id="glow" class="flex flex-col gap-5">
       <CaseCompare />
+      <MailSignup />
     </section>
   </div>
 </template>
