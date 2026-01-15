9c0b6faf61e02a5ae27345f0336376e1f5d4bd80
Wren <155106259+wryneth@users.noreply.github.com>
2026-01-14 17:51:11 -0700

    Add contact section

--- src/components/pages/Media.vue
+++ src/components/pages/Media.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import About from "../About.vue";
+import Contact from "../Contact.vue";
 </script>
 
 <template>
   <div class="mx-5 mb-5 flex flex-col gap-5">
     <About />
+    <Contact />
   </div>
 </template>
 
