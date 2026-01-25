e14962cc7939fc9bc61c5fb6d48a5fc7d93ab36f
Wren <155106259+wryneth@users.noreply.github.com>
2026-01-25 11:26:13 -0700

    Add bio text

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,31 +4,31 @@ import Social from './Social.vue';
 
 <template>
     <div class="w-full overflow-hidden card bg-base-300 items-center">
-      <div class="hero-content flex-col lg:flex-row gap-20 p-15">
+      <div class="hero-content flex-col lg:flex-row gap-15 p-15">
         <label class="swap swap-flip">
           <input type="checkbox"/>
-          <div class="swap-on hover-3d">
-            <img class="w-lg rounded-lg shadow-2xl mask mask-hexagon" src="/images/headshot-unhinged.webp" alt="unhinged Wren"/>
-          </div>
-          <div class="swap-off hover-3d">
-            <img class="w-lg rounded-lg shadow-2xl mask mask-hexagon" src="/images/headshot.webp" alt="wren"/>
-          </div>
+          <img class="swap-on hover-3d w-lg rounded-lg shadow-2xl mask mask-hexagon" src="/images/headshot-unhinged.webp" alt="unhinged Wren"/>
+          <img class="swap-off hover-3d w-lg rounded-lg shadow-2xl mask mask-hexagon" src="/images/headshot.webp" alt="Wren"/>
         </label>
         <div class="ms-5">
           <h1 class="text-6xl mb-3 font-bold font-calistoga">About</h1>
           <h2 class="text-4xl mb-1 font-bold font-calistoga">Wren Allison-Bunnell</h2>
           <h2 class="text-2xl font-bold font-calistoga mb-5">(They/them)</h2>
-          <div class="lg:w-150 md:w-full sm:w-full flex flex-col gap-3 md:pe-20 pe-10">
-            <p class="py-5 font-ubuntu">
-              Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem
-              placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
-              vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc
-              posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra
-              inceptos himenaeos.
-            </p>
+          <div class="lg:w-150 md:w-full sm:w-full flex flex-col md:pe-20 pe-10">
+            <div class="py-5 font-ubuntu text-xl flex flex-col gap-3">
+              <p>
+                Greetings! I'm Wren and I my mission is to help people learn about technology.
+              </p>
+              <p>
+                I have a background in making things, having previously run a makerspace at a museum,
+                so whether it be through video or physical items, I aim to apply everything I've learned in that space to other curious people.
+              </p>
+              <p>
+                High production value is extremely important to me, so I'm always looking for new ways to improve my work, and take the utmost care in my craft.
+              </p>
+            </div>
             <div class="lg:mt-5 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 gap-5">
-              <Social />
-              <a role="button" class="btn btn-neutral w-full dark:btn-primary">Button</a>
+              <Social/>
             </div>
           </div>
         </div>
