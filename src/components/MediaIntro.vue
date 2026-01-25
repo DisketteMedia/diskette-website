b38d496d621202b39bb1c9afa8bb032f399e3f86
Wren <155106259+wryneth@users.noreply.github.com>
2026-01-25 10:50:41 -0700

    Update MediaIntro.vue

--- src/components/MediaIntro.vue
+++ src/components/MediaIntro.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
-
+import {Icon} from '@iconify/vue'
 </script>
 
 <template>
-
   <div class="w-full flex items-center">
-    <div class="rounded-2xl md:h-200 object-cover h-150 carousel carousel-center">
-      <img src="/images/portfolio/bike.webp" alt="bike equipment unloading">
-      <img src="/images/portfolio/tub.webp" alt="the bathroom with ">
-      <img src="/images/portfolio/studio.webp" alt="diskette studio">
-      <img src="/images/portfolio/camSetup.webp" alt="Wren setting up a camera for shooting">
+    <div class="md:h-200 h-150 relative">
+      <div class="rounded-2xl md:h-200 object-cover h-150 carousel carousel-center">
+        <img src="/images/portfolio/bike.webp" alt="bike equipment unloading">
+        <img src="/images/portfolio/tub.webp" alt="the bathroom with ">
+        <img src="/images/portfolio/studio.webp" alt="diskette studio">
+        <img src="/images/portfolio/camSetup.webp" alt="Wren setting up a camera for shooting">
+      </div>
+      <div class="absolute right-3 bottom-3 flex gap-3 items-center">
+        <span class="font-ubuntu font-black text-2xl text-white">Scroll</span>
+        <Icon icon="pixel:arrow-circle-right-solid" color="white" height="35px"/>
+      </div>
     </div>
     <div class="me-5 absolute flex justify-center items-center ">
       <div class="w-full px-15">
@@ -20,9 +25,7 @@
         </picture>
         <div class="gap-2 justify-center flex my-5">
           <a href="#contact" role="button" class="btn btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Let's talk</a>
-          <a href="https://store.diskettekitchen.com/collections/assembled-lanterns"
-             role="button" class="btn btn-neutral sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">What we've done
-          </a>
+          <a href="#portfolio" role="button" class="btn btn-neutral sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">What we've done</a>
         </div>
       </div>
     </div>
