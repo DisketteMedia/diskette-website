5e78604c2fcfbe3d020989f52ca5d672bbecf5f7
Wren <155106259+wryneth@users.noreply.github.com>
2026-01-25 10:19:39 -0700

    Change intro section for DM

--- src/components/MediaIntro.vue
+++ src/components/MediaIntro.vue
@@ -3,27 +3,26 @@
 </script>
 
 <template>
-  <div class="w-full">
-    <div class="card bg-base-300 p-5 md:p-10 lg:p-5">
-      <div class="flex flex-col lg:flex-row gap-5">
-        <div class="grid grid-cols-2 place-items-center">
-          <img src="/images/portfolio/bike.webp" alt="bike equipment unloading" class="rounded-tl-md object-cover h-80">
-          <img src="/images/portfolio/tub.webp" alt="the bathroom with " class="rounded-tr-md object-cover h-80">
-          <img src="/images/portfolio/studio.webp" alt="diskette studio" class="rounded-bl-md object-cover h-80">
-          <img src="/images/portfolio/camSetup.webp" alt="Wren setting up a camera for shooting" class="rounded-br-md object-cover h-80">
-        </div>
-        <div class="pb-5 lg:p-10">
-          <img src="/mediaWide.svg" class="mb-5" alt="Diskette Kitchen logo">
-          <h2 class="text-4xl font-bold font-calistoga mb-5">Dedicated to the craft of video and </h2>
-          <p class="lg:w-200 md:w-full text-2xl sm:w-full flex flex-col gap-3 md:pe-20 pe-10 py-5 font-ubuntu">
-            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem
-            placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
-            vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc
-            posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra
-            inceptos himenaeos.
-          </p>
-          <p class="text-xl font-ubuntu mt-5">Interested in a a partnership? Let's talk</p>
-          <a role="button" class="btn btn-neutral dark:btn-primary mt-5" href="#contact">Contact me</a>
+
+  <div class="w-full flex items-center">
+    <div class="rounded-2xl md:h-200 object-cover h-150 carousel carousel-center">
+      <img src="/images/portfolio/bike.webp" alt="bike equipment unloading">
+      <img src="/images/portfolio/tub.webp" alt="the bathroom with ">
+      <img src="/images/portfolio/studio.webp" alt="diskette studio">
+      <img src="/images/portfolio/camSetup.webp" alt="Wren setting up a camera for shooting">
+    </div>
+    <div class="me-5 absolute flex justify-center items-center ">
+      <div class="w-full px-15">
+        <picture>
+          <source srcset="/mediaWide.svg" media="(max-width: 200px)">
+          <source srcset="/mediaStack.svg" media="(max-width: 1200px)">
+          <img src="/mediaWide.svg" alt="Diskette Media" class="drop-shadow-md/30 drop-shadow-amber-100 w-full">
+        </picture>
+        <div class="gap-2 justify-center flex my-5">
+          <a href="#contact" role="button" class="btn btn-primary sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Let's talk</a>
+          <a href="https://store.diskettekitchen.com/collections/assembled-lanterns"
+             role="button" class="btn btn-neutral sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">What we've done
+          </a>
         </div>
       </div>
     </div>
