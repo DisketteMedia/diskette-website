782934831d70468003ccc92a7fedddcda101e7ca
Wren <155106259+wryneth@users.noreply.github.com>
2026-01-25 11:28:38 -0700

    Remove placeholder as placeholder

--- src/components/KitchenIntro.vue
+++ src/components/KitchenIntro.vue
@@ -10,11 +10,13 @@
         <img src="/kitchenWide.svg" class="mb-5" alt="Diskette Kitchen logo">
         <h2 class="text-4xl font-bold font-calistoga mb-5">High quality videos about technology from past to present</h2>
         <p class="lg:w-200 md:w-full sm:w-full flex flex-col gap-3 md:pe-20 pe-10 text-2xl py-5 font-ubuntu">
+          <!--
             Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem
             placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
             vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc
             posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra
             inceptos himenaeos.
+            -->
         </p>
         <div class="flex gap-5">
           <a role="button" class="btn btn-neutral dark:btn-primary" href="https://youtube.com/@diskettekitchen">Watch now</a>
