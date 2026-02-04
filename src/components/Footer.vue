aa4c886e838e2772b7d5613db6390dabf928447e
Wren <155106259+wryneth@users.noreply.github.com>
2026-02-04 14:13:56 -0700

    Update themes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,13 +15,13 @@ const scrollToTop = () => {
 <template>
   <div class="mx-5 mb-5 rounded-2xl flex bg-base-300 justify-between p-4">
     <div class="flex items-center">
-      <button @click="scrollToTop" class="btn">
-        <Icon icon="pixel:arrow-circle-up-solid" />Back to top
+      <button @click="scrollToTop" class="btn btn-dash">
+        <Icon icon="pixel:arrow-circle-up-solid"/>Back to top
       </button>
       <p class="ms-4">Copyright ©2026 Diskette Media</p>
     </div>
     <div class="flex items-center gap-3">
-      <a href="https://github.com/DisketteMedia/website" target="_blank" class="me-2 text-primary underline">View source</a>
+      <a href="https://github.com/DisketteMedia/website" target="_blank" class="me-2.5 underline">View source</a>
       <Social class="hidden md:flex"/>
     </div>
   </div>
